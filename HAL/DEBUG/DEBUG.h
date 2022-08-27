1aeef27c51a74a1873d4ebc03371eabd6df157a9
Mohammed Hisham <mohammed.hisham171297@gmail.com>
2022-08-27 21:20:53 +0200

    Merge debug branch into main

--- HAL/DEBUG/DEBUG.h
+++ HAL/DEBUG/DEBUG.h
@@ -7,10 +7,34 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-void DEBUG_init(void);
-void DEBUG_put_sint(int32_t l32_num);
-void DEBUG_put_uint(uint32_t u32_num);
-void DEBUG_put_str(const char* str);
-void DEBUG_put_reg(uint32_t u32_reg, uint8_t mode);
+typedef enum debug_ports_t
+{
+	DEBUG_PORT1 = USART1,
+	DEBUG_PORT2,
+	DEBUG_PORT3,
+	DEBUG_PORT_COUNT,
+} DEBUG_PORT;
+
+#ifdef DEBUG
+void DEBUG_init(uint8_t port);
+void DEBUG_putc(uint8_t port, char ch);
+void DEBUG_puts(uint8_t port, const char* ch);
+void DEBUG_putline(uint8_t port, const char* ch);
+void DEBUG_put_sint(uint8_t port, int32_t n);
+void DEBUG_put_uint(uint8_t port, uint32_t n);
+void DEBUG_put_hex(uint8_t port, uint32_t n);
+void DEBUG_put_hex32(uint8_t port, uint32_t n);
+void DEBUG_put_bin8(uint8_t port, uint32_t n);
+#else
+#define DEBUG_init(...)
+#define DEBUG_putc(...)
+#define DEBUG_puts(...)
+#define DEBUG_putline(...)
+#define DEBUG_put_sint(...)
+#define DEBUG_put_uint(...)
+#define DEBUG_put_hex(...)
+#define DEBUG_put_hex32(...)
+#define DEBUG_put_bin8(...)
+#endif
 
 #endif
--- HAL/DEBUG/DEBUG_program.c
+++ HAL/DEBUG/DEBUG_program.c
@@ -3,63 +3,133 @@
 #include "GPIO/GPIO_interface.h"
 #include "USART/USART_interface.h"
 
+#include <string.h>
+
 #define BUF_SIZE 64
 
-void DEBUG_init(void)
+#ifdef DEBUG
+void DEBUG_init(uint8_t port)
 {
-	RCC_voidEnableClock(RCC_APB2, 14);
-	GPIO_voidSetPinMode(GPIOA, PIN9, OUTPUT_10MHZ_AF_PUSH_PULL);
-        GPIO_voidSetPinMode(GPIOA, PIN10, INPUT_FLOAT);
-	USART_voidInitSerial(USART1, 13, USART_TX_RX, USART_8_BITS, USART_STOP_1, USART_NO_PARITY);
-}
-
-void DEBUG_put_str(const char* str)
-{
-	if(str != NULL)
+	switch(port)
 	{
-		USART_voidTransmitString(USART1, str);
-		USART_voidTransmitString(USART1, "\n\r");
-	}
-}
-
-void DEBUG_put_sint(int32_t l32_num)
-{
-	char buffer[BUF_SIZE];
-
-	itoa(l32_num, buffer, 10);
-
-	buffer[BUF_SIZE - 1] = '\0';
-	USART_voidTransmitString(USART1, buffer);
-	USART_voidTransmitString(USART1, "\n\r");
-}
-
-void DEBUG_put_uint(uint32_t u32_num)
-{
-	char buffer[BUF_SIZE];
-
-	utoa(u32_num, buffer, 10);
-
-	buffer[BUF_SIZE - 1] = '\0';
-	USART_voidTransmitString(USART1, buffer);
-	USART_voidTransmitString(USART1, "\n\r");
-}
-
-void DEBUG_put_reg(uint32_t u32_reg, uint8_t mode)
-{
-	char buffer[BUF_SIZE];
-
-	switch(mode)
-	{
-		case 'x':
-			itoa(u32_reg, buffer, 16);
+		case DEBUG_PORT1:
+			RCC_voidEnableClock(RCC_APB2, 14);
+			GPIO_voidSetPinMode(GPIOA, PIN9, OUTPUT_10MHZ_AF_PUSH_PULL);
+		       	GPIO_voidSetPinMode(GPIOA, PIN10, INPUT_FLOAT);
 			break;
-		case 'b':
-			itoa(u32_reg, buffer, 2);
+		case DEBUG_PORT2:
+			/* TODO */
+			break;
+		case DEBUG_PORT3:
+			/* TODO */
 			break;
 		default:
-			buffer[0] = '\0';
+			return;
 	}
 
-	USART_voidTransmitString(USART1, buffer);
-	USART_voidTransmitString(USART1, "\n\r");
+	USART_voidInitSerial(port, 13, USART_TX_RX, USART_8_BITS, USART_STOP_1, USART_NO_PARITY);
 }
+
+void DEBUG_putc(uint8_t port, char ch)
+{
+	if(port < DEBUG_PORT_COUNT)
+		USART_voidTransmitByte(port, ch);
+}
+
+void DEBUG_puts(uint8_t port, const char* str)
+{
+	if(port < DEBUG_PORT_COUNT)
+		USART_voidTransmitString(port, str);
+}
+
+void DEBUG_putline(uint8_t port, const char* str)
+{
+	if(port < DEBUG_PORT_COUNT)
+	{
+		USART_voidTransmitString(port, str);
+		USART_voidTransmitString(port, "\n\r");
+	}
+}
+
+void DEBUG_put_sint(uint8_t port, int32_t n)
+{
+	char buf[BUF_SIZE];
+
+	if(port < DEBUG_PORT_COUNT)
+	{
+		itoa(n, buf, 10);
+		USART_voidTransmitString(port, buf);
+		USART_voidTransmitString(port, "\n\r");
+	}
+}
+
+void DEBUG_put_uint(uint8_t port, uint32_t n)
+{
+	char buf[BUF_SIZE];
+
+        if(port < DEBUG_PORT_COUNT)
+        {
+                utoa(n, buf, 10);
+                USART_voidTransmitString(port, buf);
+		USART_voidTransmitString(port, "\n\r");
+        }
+}
+
+void DEBUG_put_hex(uint8_t port, uint32_t n)
+{
+	char buf[BUF_SIZE];
+
+        if(port < DEBUG_PORT_COUNT)
+        {
+                utoa(n, buf, 16);
+                USART_voidTransmitString(port, "0x");
+                USART_voidTransmitString(port, buf);
+		USART_voidTransmitString(port, "\n\r");
+        }
+}
+
+void DEBUG_put_hex32(uint8_t port, uint32_t n)
+{
+
+	char buf[BUF_SIZE];
+	uint8_t i;
+	uint8_t len;
+
+        if(port < DEBUG_PORT_COUNT)
+        {
+                utoa(n, buf, 16);
+                USART_voidTransmitString(port, "0x");
+
+		len = strlen(buf);
+		for(i = len; i < 8; i++)
+		{
+			USART_voidTransmitByte(port, '0');
+		}
+
+                USART_voidTransmitString(port, buf);
+		USART_voidTransmitString(port, "\n\r");
+	}
+}
+
+void DEBUG_put_bin8(uint8_t port, uint32_t n)
+{
+	char buf[BUF_SIZE];
+	uint8_t i;
+	uint8_t len;
+
+        if(port < DEBUG_PORT_COUNT)
+        {
+                utoa(n, buf, 2);
+                USART_voidTransmitString(port, "0b");
+
+		len = strlen(buf);
+		for(i = len; i < 8; i++)
+		{
+			USART_voidTransmitByte(port, '0');
+		}
+                USART_voidTransmitString(port, buf);
+		USART_voidTransmitString(port, "\n\r");
+	}
+}
+
+#endif
