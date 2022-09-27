50cc17c32165b3d0f52e73834f4acce0599a959b
Mohammed Hisham <mohammed.hisham171297@gmail.com>
2022-09-27 20:52:17 +0200

    PWR initial code

---
+++ MCAL/PWR/PWR_interface.h
@@ -0,0 +1,7 @@
+#ifndef PWR_INTERFACE_H
+#define PWR_INTERFACE_H
+
+void PWR_disable_backup_protection(void);
+void PWR_enable_backup_protection(void);
+
+#endif
---
+++ MCAL/PWR/PWR_private.h
@@ -0,0 +1,10 @@
+#ifndef PWR_PRIVATE_H
+#define PWR_PRIVATE_H
+
+#define PWR_BASE (0x40007000)
+
+#define PWR_CR	((*(volatile uint32_t*)) PWR_BASE + 0x00)
+#define PWR_CSR ((*(volatile uint32_t*)) PWR_BASE + 0x04)
+
+
+#endif
